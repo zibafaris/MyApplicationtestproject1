aec1d9a1dcb4f7fcd40dd7e09bfd11bfe1780e6c
ziba <zfaris@sepehrpay.com>
2019-10-07 17:17:54 +0330

    first commit

---
+++ app/src/main/java/com/zibafaris/myapplicationtestproject/MainActivity.java
@@ -0,0 +1,51 @@
+package com.zibafaris.myapplicationtestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MyTag","onCreate");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MyTag","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MyTag","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MyTag","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MyTag","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MyTag","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MyTag","onRestart");
+    }
+
+}
